15d351e6454e4af0ec4892cfdebcec2131825a0f
Md Rony Rahman <167361775+Rony1509@users.noreply.github.com>
2025-01-22 07:21:41 +0600

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,47 +1,116 @@
-#include<iostream>
-#include<fstream>
-#include<stdio.h>
+  #include <iostream>
+#include <string>
+
+#include <thread>
+#include <mutex>
+#include <cstdlib>
+#include <cstring>
+
 #include <unistd.h>
 #include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdarg.h>
 
-using namespace std;
+#define BUF_SIZE 1024
+#define SERVER_PORT 5208
+#define IP "127.0.0.1"
 
-class Server_socket{
-    fstream file;
+void send_msg(int sock);
+void recv_msg(int sock);
 
-    int PORT;
-    
-    int general_socket_descriptor;
-    int new_socket_descriptor;
+int output(const char *arg, ...);
+int error_output(const char *arg, ...);
 
-    struct sockaddr_in address;
-    int address_length;
+void error_handling(const std::string &message);
 
-    public:
-        Server_socket(){
-            create_socket();
-            PORT = 8050;
+std::string name = "DEFAULT";
+std::string msg;
 
-            address.sin_family = AF_INET; 
-            address.sin_addr.s_addr = INADDR_ANY; 
-            address.sin_port = htons( PORT );
-            address_length = sizeof(address);
 
-            bind_socket();
-            set_listen_set();
-            accept_connection();
+int main(int argc, const char **argv, const char **envp) {
+    int sock;
+    struct sockaddr_in serv_addr;
 
-            file.open(".//Data//Server//server_text.txt", ios::in | ios::binary);
-            if(file.is_open()){
-                cout<<"[LOG] : File is ready to Transmit.\n";
-            }
-            else{
-                cout<<"[ERROR] : File loading failed, Exititng.\n";
-                exit(EXIT_FAILURE);
-            }
+    if (argc != 2) {
+        error_output("Usage : %s <Name> \n", argv[0]);
+        exit(1);
+    }
+
+    name = "[" + std::string(argv[1]) + "]";
+
+
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == -1) {
+
+        error_handling("socket() failed!");
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(IP);
+    serv_addr.sin_port = htons(SERVER_PORT);
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("connect() failed!");
+    }
+
+    std::string my_name = "#new client:" + std::string(argv[1]);
+    send(sock, my_name.c_str(), my_name.length() + 1, 0);
+
+    std::thread snd(send_msg, sock);
+    std::thread rcv(recv_msg, sock);
+
+    snd.join();
+    rcv.join();
+
+    close(sock);
+
+    return 0;
+}
+
+void send_msg(int sock) {
+    while (1) {
+        getline(std::cin, msg);
+        if (msg == "Quit" || msg == "quit") {
+            close(sock);
+            exit(0);
         }
+        std::string name_msg = name + " " + msg;
+        send(sock, name_msg.c_str(), name_msg.length() + 1, 0);
+    }
+}
 
-       
+void recv_msg(int sock) {
+    char name_msg[BUF_SIZE + name.length() + 1];
+    while (1) {
+        int str_len = recv(sock, name_msg, BUF_SIZE + name.length() + 1, 0);
+        if (str_len == -1) {
+            exit(-1);
+        }
+        std::cout << std::string(name_msg) << std::endl;
+
+    }
+}
+
+int output(const char *arg, ...) {
+    int res;
+    va_list ap;
+    va_start(ap, arg);
+    res = vfprintf(stdout, arg, ap);
+    va_end(ap);
+    return res;
+}
+
+int error_output(const char *arg, ...) {
+    int res;
+    va_list ap;
+    va_start(ap, arg);
+    res = vfprintf(stderr, arg, ap);
+    va_end(ap);
+    return res;
+}
+
+void error_handling(const std::string &message) {
+    std::cerr << message << std::endl;
+    exit(1);
+}

374109dd4059a0bc24d03964cdb24b82e526ff49
Md Rony Rahman <167361775+Rony1509@users.noreply.github.com>
2025-01-29 13:57:15 +0600

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,4 +1,4 @@
- #include <iostream>
+#include <iostream>
 #include <cstring>
 #include <thread>
 #include <mutex>
@@ -8,6 +8,8 @@
 #include <arpa/inet.h>
 #include <unordered_map>
 #include <stdarg.h>
+#include <fstream>
+#include <sstream>
 
 #define SERVER_PORT 5208
 #define BUF_SIZE 1024
@@ -15,6 +17,8 @@
 
 void handle_clnt(int clnt_sock);
 void send_msg(const std::string &msg);
+void receive_file(int clnt_sock, const std::string &filename);
+void send_file(int clnt_sock, const std::string &filename);
 int output(const char *arg,...);
 int error_output(const char *arg,...);
 void error_handling(const std::string &message);
@@ -41,7 +45,7 @@ int main(int argc, const char **argv, const char **envp) {
     if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
         error_handling("bind() failed!");
     }
-    printf("the server is running on port %d\n", SERVER_PORT);
+    printf("The server is running on port %d\n", SERVER_PORT);
 
     if (listen(serv_sock, MAX_CLNT) == -1) {
         error_handling("listen() error!");
@@ -81,10 +85,10 @@ void handle_clnt(int clnt_sock) {
                 char name[20];
                 std::strcpy(name, msg + 12);
                 if (clnt_socks.find(name) == clnt_socks.end()) {
-                    output("the name of socket %d: %s\n", clnt_sock, name);
+                    output("The name of socket %d: %s\n", clnt_sock, name);
                     clnt_socks[name] = clnt_sock;
                 } else {
-                    std::string error_msg = std::string(name) + " exists already. Please quit and enter with another name!";
+                    std::string error_msg = std::string(name) + " already exists. Please quit and enter with another name!";
                     send(clnt_sock, error_msg.c_str(), error_msg.length() + 1, 0);
                     mtx.lock();
                     clnt_cnt--;
@@ -109,9 +113,9 @@ void handle_clnt(int clnt_sock) {
         }
         clnt_cnt--;
         mtx.unlock();
-        leave_msg = "client " + name + " leaves the chat room";
+        leave_msg = "Client " + name + " leaves the chat room";
         send_msg(leave_msg);
-        output("client %s leaves the chat room\n", name.c_str());
+        output("Client %s leaves the chat room\n", name.c_str());
         close(clnt_sock);
     } else {
         close(clnt_sock);
@@ -127,7 +131,7 @@ void send_msg(const std::string &msg) {
         std::string receive_name = msg.substr(first_space + 2, space - first_space - 2);
         std::string send_name = msg.substr(1, first_space - 2);
         if (clnt_socks.find(receive_name) == clnt_socks.end()) {
-            std::string error_msg = "[error] there is no client named " + receive_name;
+            std::string error_msg = "[Error] There is no client named " + receive_name;
             send(clnt_socks[send_name], error_msg.c_str(), error_msg.length() + 1, 0);
         } else {
             send(clnt_socks[receive_name], msg.c_str(), msg.length() + 1, 0);
@@ -139,6 +143,46 @@ void send_msg(const std::string &msg) {
         }
     }
     mtx.unlock();
+}
+
+
+void receive_file(int clnt_sock, const std::string &filename) {
+    std::ofstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "[Error] Failed to create file\n";
+        return;
+    }
+
+    char buffer[BUF_SIZE];
+    int bytes_received;
+    while ((bytes_received = recv(clnt_sock, buffer, BUF_SIZE, 0)) > 0) {
+        file.write(buffer, bytes_received);
+        if (bytes_received < BUF_SIZE) {
+            break;  
+        }
+    }
+
+    file.close();
+    std::string msg = "File received successfully: " + filename;
+    send(clnt_sock, msg.c_str(), msg.length() + 1, 0);
+}
+
+
+void send_file(int clnt_sock, const std::string &filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "[Error] Failed to open file\n";
+        return;
+    }
+
+    char buffer[BUF_SIZE];
+    while (file.read(buffer, BUF_SIZE) || file.gcount() > 0) {
+        send(clnt_sock, buffer, file.gcount(), 0);
+    }
+
+    file.close();
+    std::string msg = "File sent successfully: " + filename;
+    send(clnt_sock, msg.c_str(), msg.length() + 1, 0);
 }
 
 int output(const char *arg, ...) {
@@ -158,8 +202,8 @@ int error_output(const char *arg, ...) {
     va_end(ap);
     return res;
 }
-void error_handling(const std::string &message) {
 
+void error_handling(const std::string &message) {
     std::cerr << message << std::endl;
     exit(1);
 }
